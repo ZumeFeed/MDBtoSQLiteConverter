00bfcd8f9dcc367170fc1ecabd33231ced2e8bbd
zume <zumefeed@mail.ru>
2024-08-15 18:04:08 +0300

    added workgroup information file

--- ConverterMDB/Program.cs
+++ ConverterMDB/Program.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data;
 using System.Data.OleDb;
 using System.Data.SQLite;
 using System.IO;
-using System.Runtime.ConstrainedExecution;
-using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using System.Text.Json.Nodes;
 
 
 namespace ConverterMDB
@@ -17,7 +12,7 @@ namespace ConverterMDB
     {
         public static void createTable(SQLiteConnection connectionSQLite, ref DataTable dataTable, string tableName)
         {
-            using (SQLiteCommand command = new SQLiteCommand(connectionSQLite))
+            using (SQLiteCommand command = new SQLiteCommand(connectionSQLite)) // Можно заменить string на stringBuilder
             {
                 command.CommandText = "CREATE TABLE [" + tableName + "] (\n";
                 for (int i = 0; i < dataTable.Columns.Count; i++)
@@ -30,8 +25,6 @@ namespace ConverterMDB
                             command.CommandText += " integer";
                             break;
                         case "System.DateTime":
-                            //command.CommandText += " char (50)";
-                            //break;
                         case "System.String":
                         default:
                             command.CommandText += " text";
@@ -39,12 +32,12 @@ namespace ConverterMDB
                     } // Поменять формат DateTime
 
                     if (i == 0) command.CommandText += " PRIMARY KEY AUTOINCREMENT NOT NULL, \n";
-                    else if (i < dataTable.Columns.Count-1) command.CommandText += ", \n";
+                    else if (i < dataTable.Columns.Count - 1) command.CommandText += ", \n";
                     else command.CommandText += ");";
                 }
-                Console.WriteLine(command.CommandText+"\n");
+                //Console.WriteLine(command.CommandText+"\n");
                 command.CommandType = CommandType.Text;
-                command.ExecuteNonQuery();
+                command.ExecuteNonQuery(); // Автоматическое создание таблицы
 
                 string dataRowString = "", dataListString = "";
                 int num = 0;
@@ -75,7 +68,6 @@ namespace ConverterMDB
                     dataListString += "(" + dataRowString + ")";
                     if (num < dataTable.Rows.Count)
                         dataListString += ", \n";
-
                 }
 
                 command.CommandText = "INSERT INTO [" + tableName + "] \n(";
@@ -87,30 +79,40 @@ namespace ConverterMDB
                 command.CommandText += ")\nVALUES " + dataListString + "; ";
                 //Console.WriteLine(command.CommandText);
 
-                command.ExecuteNonQuery();
+                command.ExecuteNonQuery(); // Автоматическое заполнение таблицы
             }
         }
 
         static int Main(string[] args)
         {
+
             string filePath = "", fileName = "";
             string conStrAccess = "", conStrSQLite = "";
             OleDbConnection connectionAccess;
             SQLiteConnection connectionSQLite;
 
-            int index = 0;
-            while (true) // Указать путь к файлу .mdb и проверить соединение
+
+            string fileWorkgroupPath = "", login = "", password = "";
+
+            if (File.Exists("ConnectionData.json"))
             {
-                Console.WriteLine("Path to file:");
-                Console.Write(" > ");
+                string jsonString = File.ReadAllText("ConnectionData.json");
+                JsonObject jar = JsonNode.Parse(jsonString).AsObject(); ;
+                fileWorkgroupPath = jar["fileWorkgroupPath"].ToString();
+                login = jar["login"].ToString();
+                password = jar["password"].ToString();
+            }
+
+            int index = 0;
+            while (true) // Указать путь к файлу .mdb и проверить соединение //filePath = @"E:\gate_db\events\n180910.mdb";
+            {
+                Console.Write("Путь к файлу: \n > ");
                 filePath = Console.ReadLine();
-                //filePath = @"E:\gate_db\events\n180910.mdb";
 
                 if (filePath.Contains("\\") && filePath.Contains(".mdb"))
                 {
-                    conStrAccess = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + ";" +
-                        "Jet OLEDB:System Database=E:\\gate_db\\Gate.mdw;User ID=gate;Password=gate;";
-                    index = filePath.LastIndexOf("\\") + 1;
+                    conStrAccess = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath +
+                        ";Jet OLEDB:System Database=" + fileWorkgroupPath + ";User ID=" + login + ";Password=" + password + ";";
                     connectionAccess = new OleDbConnection(conStrAccess);
                     try
                     {
@@ -121,16 +123,13 @@ namespace ConverterMDB
                         Console.WriteLine(ex.Message);
                         continue;
                     }
+                    index = filePath.LastIndexOf("\\") + 1;
                     fileName = filePath.Substring(index, filePath.Length - index - 4);
                     filePath = filePath.Substring(0, index);
                     break;
                 }
                 else
-                {
-                    filePath = "";
-                    fileName = "";
-                    Console.WriteLine("Not a file .mdb");
-                }
+                    Console.WriteLine("Указанный файл не .mdb\n");
             }
 
             if (!Directory.Exists(filePath + "converted"))
@@ -138,7 +137,7 @@ namespace ConverterMDB
                 Directory.CreateDirectory(filePath + "converted");
             }
 
-            if (!File.Exists(filePath + "converted\\" + fileName + ".sqlite")) // Create DB
+            if (!File.Exists(filePath + "converted\\" + fileName + ".sqlite")) // Создание базы данных
             {
                 SQLiteConnection.CreateFile(filePath + "converted\\" + fileName + ".sqlite");
                 conStrSQLite = "Data Source=" + filePath + "converted\\" + fileName + ".sqlite;Version=3;";
@@ -158,47 +157,48 @@ namespace ConverterMDB
 
                 DataTable dataTable = new DataTable();
                 DataTable schemeTable = connectionAccess.GetSchema("Tables");
+                OleDbDataAdapter adapter = new OleDbDataAdapter();
                 string commSelectTable = "";
                 const int tableNamesColumn = 2;
-                OleDbDataAdapter adapter = new OleDbDataAdapter();
-
-                Console.WriteLine("В файле " + fileName + " найдено " + schemeTable.Rows.Count + " таблиц(ы).");
                 int countConvertedTable = 0, countBlockedTable = 0, countSystemTable = 0;
                 string tableName = "";
-                for (int i = 0; i < schemeTable.Rows.Count; i++) // Считывание и проверка доступа к таблицам с данными
+
+                Console.WriteLine("В файле " + fileName + " найдено " + schemeTable.Rows.Count + " таблиц(ы).");
+                for (int i = 0; i < schemeTable.Rows.Count; i++) // Обработка таблиц
                 {
                     tableName = schemeTable.Rows[i].ItemArray[tableNamesColumn].ToString();
                     commSelectTable = "SELECT * FROM " + tableName;
-                    if (tableName.Contains("MSys"))
+
+                    if (tableName.Contains("MSys")) // Пропуск системных таблиц
                     {
                         countSystemTable++;
                         continue;
                     }
-                    try
+
+                    try // Исключение - права доступа
                     {
                         adapter = new OleDbDataAdapter(commSelectTable, connectionAccess);
-                        dataTable = new DataTable();
                         adapter.Fill(dataTable);
                     }
-                    catch (Exception ex)
+                    catch (Exception ex) // Можно сделать log-и
                     {
                         countBlockedTable++;
                         continue;
                     }
 
                     createTable(connectionSQLite, ref dataTable, tableName);
+                    dataTable = new DataTable();
                     countConvertedTable++;
                 }
 
-                Console.WriteLine("Успешно конвертировано - " + countConvertedTable + "\nНедоступно или с ошибкой - " + countBlockedTable + "\nПропущено системных - " + countSystemTable);
+                Console.WriteLine("Конвертировано - " + countConvertedTable + "\nНедоступно - " + countBlockedTable + "\nСистемные - " + countSystemTable);
                 connectionSQLite.Close();
                 connectionAccess.Close();
             }
-            else { } // Update DB
+            else { } // Обновление базы данных
 
             Console.ReadKey();
             return 0;
-
         }
     }
 }
